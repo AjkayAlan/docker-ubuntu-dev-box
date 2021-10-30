b798f4d5ddc9719a3d1f87bebc3e8a65b6d50e3f
Alan Kay <alanajkay@gmail.com>
2021-10-30 13:06:34 -0500

    Fixed paste speed

--- scripts/install-zsh.sh
+++ scripts/install-zsh.sh
@@ -29,3 +29,19 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i 's/\(^plugins=([^)]*\)/\1 zsh-autosuggestions zsh-syntax-highlighting gh/' ~/.zshrc
 source ~/.zshrc
+
+# Fix paste speed - see https://gist.github.com/magicdude4eva/2d4748f8ef3e6bf7b1591964c201c1ab
+cat <<'EOT' >> ~/.zshrc
+### Fix slowness of pastes with zsh-syntax-highlighting.zsh
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic
+}
+
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
+### Fix slowness of pastes
+EOT
