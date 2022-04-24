8a56d4caedf7f9f738f81d2a91a2e08810290e18
Alan Kay <alanajkay@gmail.com>
2022-04-24 11:56:53 -0500

    Update install-powershell-core.sh

--- scripts/install-powershell-core.sh
+++ scripts/install-powershell-core.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Install PowerShell Core and make it available
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt update
 apt install -y powershell
