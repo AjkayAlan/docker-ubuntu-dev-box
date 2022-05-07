5bdf64faa0623ef0d106ace79ba62cd264fa8888
Alan Kay <alanajkay@gmail.com>
2022-05-06 22:11:53 -0500

    is R that easy

--- scripts/install-rlang.sh
+++ scripts/install-rlang.sh
@@ -1,18 +1,5 @@
 #!/bin/zsh
 
-# Install dependencies
-apt install -y \
-    apt-transport-https \
-    build-essential \
-    ca-certificates \
-    dirmngr \
-    gnupg \
-    software-properties-common
-
-# Add CRAN
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-
 # Install R
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+apt update
 apt install -y r-base
