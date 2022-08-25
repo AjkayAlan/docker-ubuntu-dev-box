facf5aad91310201bc947f21790ba3b74a475ee8
Alan Kay <alanajkay@gmail.com>
2022-08-25 12:13:25 -0500

    Fix dotnet install

--- scripts/install-netcore.sh
+++ scripts/install-netcore.sh
@@ -3,14 +3,25 @@
 # Get LTS version
 lts_version=$(curl https://dotnetcli.blob.core.windows.net/dotnet/Sdk/LTS/latest.version)
 lts_version=${lts_version%.*}
+lts_version=${lts_version%.*}
 
-# Install .NET Core and make it available
+# Get microsoft's feed
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 
-apt update; \
-apt install -y apt-transport-https && \
-apt update && \
+# Prefer microsoft's feed
+if [ ! -f /etc/apt/preferences ]
+then
+touch /etc/apt/preferences
+tee -a /etc/apt/preferences > /dev/null <<EOT
+Package: *
+Pin: origin "packages.microsoft.com"
+Pin-Priority: 1001
+EOT
+fi
+
+# Install .NET
+apt update
 apt install -y dotnet-sdk-$lts_version
 
 # Cleanup
--- scripts/install-powershell-core.sh
+++ scripts/install-powershell-core.sh
@@ -3,6 +3,19 @@
 # Install PowerShell Core and make it available
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
+
+# Prefer microsoft's feed
+if [ ! -f /etc/apt/preferences ]
+then
+touch /etc/apt/preferences
+tee -a /etc/apt/preferences > /dev/null <<EOT
+Package: *
+Pin: origin "packages.microsoft.com"
+Pin-Priority: 1001
+EOT
+fi
+
+# Install PowerShell
 apt update
 apt install -y powershell
 
